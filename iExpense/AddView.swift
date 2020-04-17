cd5e8b35c4dc6b62c3ae68c34a1acdfae07527aa
Chris Ameter <c.public@ameters.com>
2020-04-17 14:58:25 -0600

    Finished the project.  Included work-around for Swift 5.2 bug.

---
+++ iExpense/AddView.swift
@@ -0,0 +1,57 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Chris on 4/17/20.
+//  Copyright © 2020 CodePika. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    static let types = ["Business", "Personal"]
+    
+    @State private var showingAlert = false
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text:$name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing:
+                Button("Save") {
+                    if let actualAmount = Int(self.amount) {
+                        let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                        self.expenses.items.append(item)
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        self.showingAlert = true
+                    }
+            })
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Invalid Amount"), message: Text("You entered an amount of '\(self.amount)'.  This is not a valid integer."), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,11 +6,119 @@
 //  Copyright © 2020 CodePika. All rights reserved.
 //
 
+
+
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items: [ExpenseItem] //{
+//  The following code was removed because the didSet property observer currently does NOT fire on a property with the @Published property wrapper.
+//
+//  The alternative is to either oberve the property in the stuct directly (if not using an array), OR apply .onReceive(expenses.$items) { items in } to any view that receives the notification of the update (see below for an example).
+//
+//        didSet {
+//            print("trying to write items")
+//            let encoder = JSONEncoder()
+//            if let encoded = try? encoder.encode(items) {
+//                print("writing items")
+//                UserDefaults.standard.set(encoded, forKey: "Items")
+//            }
+//        }
+//    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try?  decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
+        self.items = []
+    }
+}
+
+
+struct ExpenseStyleModifier: ViewModifier {
+    let amount: Int
+    
+    func body(content: Content) -> some View {
+        switch amount {
+        case ..<10:
+            return content
+                .foregroundColor(.red)
+        case ..<100:
+            return content
+                .foregroundColor(.black)
+        default:
+            return content
+                .foregroundColor(.green)
+        }
+    }
+}
+
+extension View {
+    func expenseStyle(amount: Int) -> some View {
+        self.modifier(ExpenseStyleModifier(amount: amount))
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text("\(item.amount)")
+                            .expenseStyle(amount: item.amount)
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(leading: EditButton(),
+                trailing: Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            }
+            .onReceive(expenses.$items) { items in
+                // This is used as a workaround to write to UserDefaults every time the items property of the expenses class is updated.
+                // This is necessary because the didSet property observer currently does NOT fire on a property with the @Published property wrapper.
+                let encoder = JSONEncoder()
+                if let encoded = try? encoder.encode(items) {
+                    UserDefaults.standard.set(encoded, forKey: "Items")
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
