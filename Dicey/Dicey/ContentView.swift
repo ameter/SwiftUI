5f04fa9c9b2eac138961c8b6cfca458023d004a6
Chris Ameter <c.public@ameters.com>
2020-05-10 11:35:08 -0600

    Beginning Dicey (final challenge project)

---
+++ Dicey/Dicey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicey
+//
+//  Created by Chris on 5/10/20.
+//  Copyright © 2020 CodePika. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
